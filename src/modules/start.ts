e788d4a3ad89d74936de3fcefb74d635f7ed6e36
Sydxyz <109050076+Sydxyz@users.noreply.github.com>
2023-05-15 20:59:14 +0530

    Update start.ts

--- src/modules/start.ts
+++ src/modules/start.ts
@@ -22,7 +22,7 @@ composer
       reply_markup: new InlineKeyboard()
         .text(ctx.t("usage-help"), "helper")
         .text("Language 🌐", "setLang").row()
-        .url("&"),"https://t.me/parallel")
+
         .url(ctx.t("updates"), "https://t.me/Bot_cracker"),
       disable_web_page_preview: true,
     });
@@ -37,7 +37,7 @@ composer.callbackQuery("mainMenu", async (ctx) => {
       reply_markup: new InlineKeyboard()
         .text(ctx.t("usage-help"), "helper")
         .text("Language 🌐", "setLang").row()
-        .url("&"),"https://t.me/parallel")
+
         .url(ctx.t("updates"), "https://t.me/bot_cracker"),
       disable_web_page_preview: true,
     },
