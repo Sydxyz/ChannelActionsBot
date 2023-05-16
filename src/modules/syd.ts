9e95be7af727d67227acb371174de854c46db9be
Sydxyz <109050076+Sydxyz@users.noreply.github.com>
2023-05-16 16:35:41 +0530

    Create syd.ts

---
+++ src/modules/syd.ts
@@ -0,0 +1,49 @@
+import { MyContext } from "../core/types.ts";
+import { addUser } from "../database/usersDb.ts";
+
+import { Composer, InlineKeyboard } from "grammy/mod.ts";
+
+const composer = new Composer<MyContext>();
+
+composer
+  .command("help", async (ctx) => {
+    if (ctx.chat.type != "private" && ctx.match == "by_BotzHub") {
+      await ctx.reply("Continue setting me up in PM!", {
+        reply_markup: new InlineKeyboard().url(
+          "Continue",
+          `https://t.me/${ctx.me.username}`,
+        ),
+      });
+      return;
+    }
+    if (ctx.chat.type != "private") return;
+    await ctx.reply(ctx.t("start-msg", { user: ctx.from!.first_name }), {
+      parse_mode: "HTML",
+      reply_markup: new InlineKeyboard()
+        .url("✚✖ Δᴅᴅ MΞ T𝕠 Y̲ᴏʊʀ CʜᴀΓ ✖✚","http://t.me/instant_approval_bot?startgroup=true").row()
+        .text(ctx.t("usage-help"), "helper")
+        .text("Language 🌐", "setLang").row()
+        .url("00", "https://t.me/parallel")
+        .text(ctx.t("demo-syd"),"heper")
+        .url(ctx.t("updates"), "https://t.me/Bot_cracker"),
+      disable_web_page_preview: true,
+    });
+    await addUser(ctx.from!.id);
+  });
+
+composer.callbackQuery("mainMenu", async (ctx) => {
+  await ctx.editMessageText(
+    ctx.t("start-msg", { user: ctx.from!.first_name }),
+    {
+      parse_mode: "HTML",
+      reply_markup: new InlineKeyboard()
+        .text(ctx.t("usage-help"), "helper")
+        .text("Language 🌐", "setLang").row()
+        .text("ctx.t("demo-syd"), "heper")
+        .url(ctx.t("updates"), "https://t.me/bot_cracker"),
+      disable_web_page_preview: true,
+    },
+  );
+});
+
+export default composer;
