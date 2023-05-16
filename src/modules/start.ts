ae2862801c049686e4085d0144658d3ec3cdcb76
Sydxyz <109050076+Sydxyz@users.noreply.github.com>
2023-05-16 13:35:34 +0530

    Update start.ts

--- src/modules/start.ts
+++ src/modules/start.ts
@@ -21,7 +21,7 @@ composer
       parse_mode: "HTML",
       reply_markup: new InlineKeyboard()
         .url("✚✖ Δᴅᴅ MΞ T𝕠 Y̲ᴏʊʀ CʜᴀΓ ✖✚","http://t.me/instant_approval_bot?startgroup=true").row()
-        .text(ctx.t("usage-help"), "helper")
+        .text(ctx.t("usage-help"), "heper")
         .text("Language 🌐", "setLang").row()
         .url("00", "https://t.me/parallel")
         .text(ctx.t("help"),"help")
